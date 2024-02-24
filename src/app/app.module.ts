e2de652b7e4db0ab7dd85f9caf5d96fd77f73b6b
ElVecchio <gilberto.vecchi@hotmail.com>
2024-02-23 21:29:30 -0300

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopMenuModule } from './components/top-menu/top-menu.module';
+import { HeadModule } from './components/head/head.module';
+import { ArticleSumModule } from './components/article-sum-component/article-sum.module';
+import { ArticleModule } from './components/article-component/article.module';
+import { HrComponent } from './components/hr/hr.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HrComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TopMenuModule,
+    HeadModule,
+    ArticleSumModule,
+    ArticleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/head/head.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeadComponent } from './head.component';
+
+
+@NgModule({
+  declarations: [ HeadComponent ],
+  exports: [ HeadComponent ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HeadModule { }
